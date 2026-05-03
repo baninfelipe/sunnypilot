a0855c0ab754e618fb436ae7a1a007f324be9af7
baninfelipe <carvalho.felipebanin@gmail.com>
2026-05-03 17:02:26 -0300

    rollback to 4096 torqueV

--- opendbc_repo/opendbc/car/honda/interface.py
+++ opendbc_repo/opendbc/car/honda/interface.py
@@ -204,7 +204,7 @@ class CarInterface(CarInterfaceBase):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate == CAR.HONDA_CITY_7G:
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4608], [0, 4608]]
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     # TODO-SP: remove when https://github.com/commaai/opendbc/pull/2687 is merged
